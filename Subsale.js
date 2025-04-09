fcea67395e3b75540217361d8916f967866ef61f
afrproperty <afrpropertys@gmail.com>
2025-04-09 18:17:55 +0800

    Update Subsale.js

--- Subsale.js
+++ Subsale.js
@@ -7,7 +7,7 @@ const properties = [
     { img: "https://afrproperty.onpay.my/media/uploads/TamanSetiaIndahPresint12-1.jpg", price: "RM 750,000", address: "Taman Setia Indah Precinct 12", details: "4 Beds | 3 Baths | 1 Storeroom | 1,300 SQFT", link: "https://afrproperty.onpay.my/order/form/subsale-tamansetiaindahprecinct12" },
     { img: "https://afrproperty.onpay.my/media/uploads/2StoreyCornerLotAustinPerdana1.jpg", price: "RM 2,300,000", address: "Taman Austin Perdana, Johor Bahru", details: "4+1 Beds | 4 Baths | 5,000+ SQFT", link: "https://afrproperty.onpay.my/order/form/subsale-2storeycornerlotaustinperdana" },
     { img: "https://afrproperty.onpay.my/media/uploads/ClusterHouseAustinHeight1.jpg", price: "RM 1,250,000", address: "Cluster House @ Austin Heights", details: "4 Beds | 4 Baths | SQFT", link: "https://afrproperty.onpay.my/order/form/subsale-clusterhouseaustinheight" },
-    { img: "https://afrproperty.onpay.my/media/uploads/ResidenceThePeak1.jpg", price: "RM 4,300,000", address: "Residence at The Peak", details: "6+1 Beds | 7 Baths | 7,916 SQFT (Built-up) | 7,089 SQFT (Land)", link: "https://afrproperty.onpay.my/order/form/subsale-residencethepeak" },
+    { img: "https://afrproperty.onpay.my/media/uploads/ResidenceThePeak1.jpg", price: "RM 4,300,000", address: "Residence at The Peak", details: "6+1 Beds | 7 Baths | 7,916 SQFT", link: "https://afrproperty.onpay.my/order/form/subsale-residencethepeak" },
     { img: "https://afrproperty.onpay.my/media/uploads/TamanSerimbunIskandarPuteri1.jpg", price: "RM 1,150,000", address: "Taman Serimbun, Iskandar Puteri", details: "4+1 Beds | 3 Baths | 1,882 SQFT", link: "https://afrproperty.onpay.my/order/form/subsale-tamanserimbuniskandarputeri" }
 ];
 
